c617135fb8dacb0a5b1a8de08683dd047081dd5b
jonas pauli <jonaspauli089@gmail.com>
2025-02-04 21:53:53 +0100

    wip: preserve root history

--- src/Trie.ts
+++ src/Trie.ts
@@ -1,14 +1,6 @@
 import { Field, Poseidon, MerkleTree, ZkProgram, UInt32, Struct, MerkleWitness, Circuit, SelfProof, Proof } from 'o1js';
 
 
-class PublicOutput extends Struct(
-    {
-        new_leaf: Field,
-        current_root: Field,
-        previous_root: Field,
-    }
-) { }
-
 // a simplified mock tree that works with field elements
 class CustomTree extends Struct({
     leafs: Array(Field)
@@ -22,43 +14,49 @@ class CustomTree extends Struct({
     }
 }
 
+class InsertPublicOutput extends Struct(
+    {
+        leaf_history: Array(Field),
+        root_history: Array(Field),
+    }
+) { }
+
 class CircuitInputs extends Struct({
     tree: CustomTree,
     root: Field,
     new_input: Field,
 }) { }
 
-const SimpleProgram = ZkProgram({
+const InsertProgram = ZkProgram({
     name: 'mina-recursive-tree-program',
     publicInput: CircuitInputs,
-    publicOutput: PublicOutput,
+    publicOutput: InsertPublicOutput,
     methods: {
-        prove: {
+        prove_insert: {
             privateInputs: [],
             async method(publicInput: CircuitInputs) {
-                let previous_root = publicInput.tree.hash();
                 let input_hash = Poseidon.hash([publicInput.new_input]);
                 let new_tree = publicInput.tree.insert(input_hash);
                 // commit the current, and previous root and the new leaf
-                return { publicOutput: new PublicOutput({ new_leaf: publicInput.new_input, current_root: new_tree.hash(), previous_root: previous_root }) };
+                return { publicOutput: new InsertPublicOutput({ leaf_history: [publicInput.new_input], root_history: [new_tree.hash()] }) };
             },
         },
-        recursive: {
+        recursive_insert: {
             privateInputs: [SelfProof],
-            async method(publicInput: CircuitInputs, previous_proof: SelfProof<CircuitInputs, PublicOutput>) {
-                let previous_root = publicInput.tree.hash();
-                previous_root.assertEquals(previous_proof.publicOutput.current_root);
+            async method(publicInput: CircuitInputs, previous_proof: SelfProof<CircuitInputs, InsertPublicOutput>) {
                 previous_proof.verify();
                 let input_hash = Poseidon.hash([publicInput.new_input]);
                 let new_tree = publicInput.tree.insert(input_hash);
+                let new_leaf_history = [...previous_proof.publicOutput.leaf_history, publicInput.new_input];
+                let new_root_history = [...previous_proof.publicOutput.root_history, new_tree.hash()];
                 // assuming the previous proof is valid, commit the current, and previous root and the new leaf
-                return { publicOutput: new PublicOutput({ new_leaf: publicInput.new_input, current_root: new_tree.hash(), previous_root: previous_root }) };
+                return { publicOutput: new InsertPublicOutput({ leaf_history: new_leaf_history, root_history: new_root_history }) };
             },
         }
     },
 });
 
-const { verificationKey } = await SimpleProgram.compile();
+const { verificationKey } = await InsertProgram.compile();
 // each leaf is an arbitrarily sized leaf's poseidon hash
 // therefore a leaf can represent any data.
 // initialize with an empty leaf for simplicity sake
@@ -69,12 +67,11 @@ let Tree = new CustomTree({ leafs: leafs });
 // hash the initial tree root
 let initial_root = Tree.hash();
 let first_input = new CircuitInputs({ tree: Tree, root: initial_root, new_input: Field(22222) })
-const { proof } = await SimpleProgram.prove(first_input);
-
+const { proof } = await InsertProgram.prove_insert(first_input);
 let new_leaf = Field(11111);
 Tree.insert(new_leaf);
 let second_input = new CircuitInputs({ tree: Tree, root: initial_root, new_input: new_leaf })
-const { proof: second_proof } = await SimpleProgram.recursive(second_input, proof);
-console.log("Second Proof: ", second_proof);
+/*const { proof: second_proof } = await SimpleProgram.recursive(second_input, proof);
+console.log("Second Proof: ", second_proof);*/
 
 
